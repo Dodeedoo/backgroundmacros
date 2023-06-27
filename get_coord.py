524210065b42d3a207b9428b1a3d6bea040dd7e8
Dodeedoo <dodoalt112312@gmail.com>
2023-06-27 00:19:03 -0700

    e

--- get_coord.py
+++ get_coord.py
@@ -3,6 +3,6 @@ import time
 
 hwndMain = win32gui.FindWindow(None, "Lunar Client 1.8.9 (v2.7.2-2324)")
 
-time.sleep(3)
+time.sleep(5)
 
 print(str(win32gui.ScreenToClient(hwndMain, win32gui.GetCursorPos())))
